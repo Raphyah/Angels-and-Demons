a11e8713ea12e22b1ea3eac65a2b7e61fc634401
Raphyah <45445230+Raphyah@users.noreply.github.com>
2019-04-21 14:11:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ var current=0;
 function next(){
 	current++;
 	globalReloadTime+=0.5;
-	if (current==1)loadFile.SCRIPT("script/game/weapons.js","body","next");
-	if (current==2)loadFile.SCRIPT("script/game/armors.js","body","next");
-	if (current==3)loadFile.SCRIPT("script/game/items.js","body","next");
-	if (current==4)loadFile.SCRIPT("script/game/skills.js","body","next");
-	if (current==5)loadFile.SCRIPT("script/game/enemies.js","body","next");
-	if (current==6)loadFile.SCRIPT("script/game/notes.js","body","next");
-	if (current==7)loadFile.SCRIPT("script/game/npcs.js","body","next");
-	if (current==7)loadFile.SCRIPT("script/game/pets.js","body","next");
+	if (current==1)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/weapons.js","body","next");
+	if (current==2)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/armors.js","body","next");
+	if (current==3)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/items.js","body","next");
+	if (current==4)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/skills.js","body","next");
+	if (current==5)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/enemies.js","body","next");
+	if (current==6)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/notes.js","body","next");
+	if (current==7)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/npcs.js","body","next");
+	if (current==7)loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/pets.js","body","next");
 }
 
 loadFile.SCRIPT("https://raphyah.github.io/angels-and-demons/script/game/players.js","body","next");
-loadFile.STYLE("style/main.css");
+loadFile.STYLE("https://raphyah.github.io/angels-and-demons/style/main.css");
